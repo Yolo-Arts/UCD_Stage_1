417e63345ad829f6fa5df497a51c6f09d03d9aa8
Yolo-Arts <125148189+Yolo-Arts@users.noreply.github.com>
2025-11-13 09:50:27 +0000

    Update Closures.rkt

--- Scheme/Closures.rkt
+++ Scheme/Closures.rkt
@@ -18,3 +18,24 @@
 
 ; closures carry around the scope. It remembers all the variables from the function it was called from.
 ; functions which hold state/context because they were created within the context and use the context.
+
+
+; define a function addX that takes a number X and returns a FUNCTION that takes some other number and adds X to it.
+;; (define myFun (addX 10))
+;; Should give a function, and I call by saying (myFun 12) and get back (+ 10 12)
+
+(define addX
+  (lambda (X)
+
+    (lambda (N)
+      (+ X N)
+    )
+  )
+  )
+
+(define myFun (addX 10))
+(myFun 12)
+
+
+; A recurisve closure?
+; Where the inner closure calls itself, even tho the inner function does not have a name.
