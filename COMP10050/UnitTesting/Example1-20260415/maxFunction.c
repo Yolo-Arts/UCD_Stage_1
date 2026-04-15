11fec97a7ba0edae95c95f1a3c06ac536d3a2ecd
Yolo-Arts <Jackteong123@gmail.com>
2026-04-15 11:18:06 +0100

    adding unit testing

---
+++ COMP10050/UnitTesting/Example1-20260415/maxFunction.c
@@ -0,0 +1,13 @@
+/*
+ * maxFunction.c
+ *
+ *  Created on: 17 Apr 2017
+ *      Author: liliana
+ */
+#include "maxFunction.h"
+
+int maxi(int i1, int i2){
+	if (i1 > i2)
+		return i1;
+	else return i2;
+}
