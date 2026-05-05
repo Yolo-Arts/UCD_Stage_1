c4a498cf6fa6b239360a0bdf60467685fe9b1204
Yolo-Arts <Jackteong123@gmail.com>
2026-05-05 11:30:57 +0100

    COMP 10050 finished studying for assignment 1

---
+++ COMP10050/Exam/Assignment-1/blurt copy 2.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX_LENGTH 80
+#define MAX_SONGS 12
+
+void update_recently_played(char recentlyPlayed[][MAX_LENGTH], char *songTitle) {
+    for (int k = 4; k > 0; k--) {
+        strcpy(recentlyPlayed[k], recentlyPlayed[k - 1]);
+    }
+    strcpy(recentlyPlayed[0], songTitle);
+}
+
+int is_recently_played(char recentlyPlayed[][MAX_LENGTH], char *songTitle) {
+    for (int i = 0; i < 5; i++) {
+        if (strcmp(recentlyPlayed[i], songTitle) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void parallelSwap(char songList[][MAX_LENGTH], char artistList[][MAX_LENGTH], int i, int j) {
+    char tempSong[MAX_LENGTH];
+    char tempArtist[MAX_LENGTH];
+
+    strcpy(tempArtist, artistList[i]);
+    strcpy(tempSong, songList[i]);
+
+    strcpy(artistList[i], artistList[j]);
+    strcpy(songList[i], songList[j]);
+
+    strcpy(artistList[j], tempArtist);
+    strcpy(songList[j], tempSong);
+}
+
+void shufflePlaylist(char songList[][MAX_LENGTH], char artistList[][MAX_LENGTH], int numOfSongs) {
+    int totalItems = numOfSongs * 2;
+
+    char poolArtists[MAX_SONGS * 2][MAX_LENGTH];
+    char poolSongs[MAX_SONGS * 2][MAX_LENGTH];
+    int used[MAX_SONGS * 2] = {0};
+
+    char resultArtists[MAX_SONGS * 2][MAX_LENGTH];
+    char resultSongs[MAX_SONGS * 2][MAX_LENGTH];
+    char recentlyPlayed[5][MAX_LENGTH];
+
+    for (int i = 0; i < 5; i++) {
+        strcpy(recentlyPlayed[i], "");
+    }
+
+    for (int i = 0; i < numOfSongs; i++) {
+        strcpy(poolArtists[i], artistList[i]);
+        strcpy(poolArtists[i + numOfSongs], artistList[i]);
+        strcpy(poolSongs[i], songList[i]);
+        strcpy(poolSongs[i + numOfSongs], songList[i]);
+    }
+
+    for (int i = totalItems - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        parallelSwap(poolSongs, poolArtists, i, j);
+    }
+
+    for (int i = 0; i < totalItems; i++) {
+        int valid_index = 0;
+        int random_index = 0;
+
+        while(valid_index == 0) {
+            random_index = rand() % totalItems;
+
+            if (used[random_index] == 0) {
+                if (is_recently_played(recentlyPlayed ,poolSongs[random_index]) == 0) {
+                    valid_index = 1;
+                }
+            }
+        }
+
+        used[random_index] = 1;
+        strcpy(resultArtists[i], poolArtists[random_index]);
+        strcpy(resultSongs[i], poolSongs[random_index]);
+
+        update_recently_played(recentlyPlayed, poolSongs[random_index]);
+    }
+}
